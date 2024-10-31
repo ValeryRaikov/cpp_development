b4d3f75aefdc3b8d2d02c22544af5b64487ac0ce
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-10-31 21:54:04 +0200

    Add lab3 code

---
+++ lab3/ex1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class Stack {
+private:
+    stack<char> customStack;
+public:
+    Stack() = default;
+
+    void push(char ch) {
+        customStack.push(ch);
+    }
+
+    void pop() {
+        if (!customStack.empty()) {
+            customStack.pop();
+        }
+    }
+
+    void loadStack() {
+        char startChar = 'a';
+        for (char ch = startChar; ch <= (startChar + 25); ++ch) {
+            customStack.push(ch);
+        }
+    }
+
+    void loadStack(int upper) {
+        while (!customStack.empty()) {
+            customStack.pop();
+        }
+
+        char startChar = upper ? 'A' : 'a';
+        for (char ch = startChar; ch <= (startChar + 25); ++ch) {
+            customStack.push(ch);
+        }
+    }
+
+    void printStack() {
+        cout << "Stack is:\n";
+        while (!customStack.empty()) {
+            cout << customStack.top() << ", ";
+            customStack.pop();
+        }
+        cout << endl;
+        }
+};
+
+int main() {
+    Stack st1;
+    st1.loadStack(1);
+    st1.printStack();
+
+    Stack st2;
+    st2.loadStack();
+    st2.printStack();
+
+    return 0;
+}
